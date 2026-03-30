406ab94e364b24ef2e6db843dae6b6d520ad4a08
Tomáš Chochola <tomaschochola@tomaschochola.cz>
2026-03-30 21:14:55 +0200

    Update: package.json | Add: src/index.js, templates/base.js

---
+++ src/index.js
@@ -0,0 +1,62 @@
+/**
+ * @file
+ * @author Tomáš Chochola <tomaschochola@tomaschochola.cz>
+ * @copyright © 2026 Tomáš Chochola <tomaschochola@tomaschochola.cz>
+ *
+ * @license CC-BY-ND-4.0
+ *
+ * @see {@link https://creativecommons.org/licenses/by-nd/4.0/} License
+ * @see {@link https://github.com/tomaschochola} GitHub Profile
+ * @see {@link https://github.com/sponsors/tomaschochola} GitHub Sponsors
+ */
+
+export class StylelintStack {
+  config;
+
+  constructor() {
+    this.config = {
+      extends: [],
+      rules: {},
+    };
+  }
+
+  get NODE_ENV() {
+    return process.env.NODE_ENV;
+  }
+
+  replace(config) {
+    this.config = { ...config };
+
+    return this;
+  }
+
+  standard(options = {}) {
+    return this.replace({
+      ...this.config,
+      extends: [
+        ...this.config.extends,
+        'stylelint-config-standard-scss',
+      ],
+      ...options,
+    });
+  }
+
+  prettier(options = {}) {
+    return this.replace({
+      ...this.config,
+      extends: [
+        ...this.config.extends,
+        'stylelint-prettier/recommended',
+      ],
+      rules: {
+        ...this.config.rules,
+        'prettier/prettier': null,
+      },
+      ...options,
+    });
+  }
+
+  build() {
+    return { ...this.config };
+  }
+}
---
+++ templates/base.js
@@ -0,0 +1,19 @@
+/**
+ * @file
+ * @author Tomáš Chochola <tomaschochola@tomaschochola.cz>
+ * @copyright © 2026 Tomáš Chochola <tomaschochola@tomaschochola.cz>
+ *
+ * @license CC-BY-ND-4.0
+ *
+ * @see {@link https://creativecommons.org/licenses/by-nd/4.0/} License
+ * @see {@link https://github.com/tomaschochola} GitHub Profile
+ * @see {@link https://github.com/sponsors/tomaschochola} GitHub Sponsors
+ */
+
+import { StylelintStack } from '@premierstacks/stylelint-stack';
+
+// eslint-disable-next-line no-restricted-exports
+export default new StylelintStack()
+  .standard()
+  .prettier()
+  .build();
